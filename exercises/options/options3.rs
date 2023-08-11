82c0154bce9a1c05a93ced8562f185f84535343d
n0thing2speak <ingnoscemihi@gmail.com>
2023-08-11 14:15:08 +0800

    options are done

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,18 +3,17 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
     y: i32,
 }
 
-fn main() {
+fn main(){
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y), //ref的意思就是不消耗some的所有权，这样上面的y之后还可以正常使用
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
